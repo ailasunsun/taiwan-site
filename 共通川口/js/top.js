112f91db99846045a9d837806641f449f341fc2e
ailasunsun <aira.5510_1z@icloud.com>
2024-07-30 12:50:56 +0900

    ローディング画面作成。今後微調整する可能性有です　by川口

--- 共通川口/js/top.js
+++ 共通川口/js/top.js
@@ -1,1 +1,26 @@
 'use strict';
+
+//ローディングdiv
+const loadingPage = document.getElementById('loading');
+
+//ロゴ
+const logo = document.getElementById('logo');
+
+//初期は透明
+logo.style.opacity = "0";
+
+//フェードインアニメーション
+window.addEventListener('load', function () {
+  logo.style.opacity = '1';
+  logo.style.transition = 'opacity 3000ms ease-in-out';
+  loadingPage.classList.add('hidden');
+  fadeOut();
+});
+
+//フェードアウトアニメーション
+function fadeOut() {
+  const hidden = document.querySelector('.hidden');
+  hidden.style.opacity = '0';
+  hidden.style.transition = 'opacity 3000ms ease-in-out 3000ms';
+}
+
