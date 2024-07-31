886a6fa9cbf7e07ff7998232ad87e9a079bd8829
bellthecatapp <bellthecatapp@gmail.com>
2024-07-31 09:19:58 +0900

    Commonに共通部分追加しました

--- 共通野島/js/test.js
+++ 共通野島/js/test.js
@@ -1,8 +1,9 @@
 'use strict';
 
+// モーダル夜市　開閉
 document.getElementById('market_btn').onclick = function () {
-    const modalWindow = document.getElementsById('modal_marketbox');
-    modalWindow.style.display = 'flex';
+    const modalWindow = document.getElementById('modal_marketbox');
+    modalWindow.style.display = 'block';
 
     const closeBtn = document.getElementById('close_btn');
     closeBtn.onclick = function () {
@@ -10,4 +11,13 @@ document.getElementById('market_btn').onclick = function () {
     }
 };
 
+// モーダル文化施設　開閉
+document.getElementById('cultural_btn').onclick = function () {
+    const modalWindow = document.getElementById('mordal_culturalbox');
+    modalWindow.style.display = 'block';
 
+    const closeBtn = document.getElementById('close_culture1');
+    closeBtn.onclick = function () {
+        modalWindow.style.display = 'none';
+    }
+};
