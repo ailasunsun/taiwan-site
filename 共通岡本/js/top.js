47da47536b9fc01a2366a06a8ff7da66f62761f0
ytk32 <gangbenyongxi55@gmail.com>
2024-08-01 14:58:40 +0900

    fds

--- 共通岡本/js/top.js
+++ 共通岡本/js/top.js
@@ -1,2 +1,11 @@
 'use strict';
 
+// document.querySelector("button").onclick=function(){
+//     document.getElementById('nav').style.display='block';
+//     document.getElementById('open').style.display='none';
+// };
+
+document.getElementById("close").onclick = function () {
+    document.getElementById('nav').style.display = 'none';
+    document.getElementById('open').style.display = 'block';
+};
