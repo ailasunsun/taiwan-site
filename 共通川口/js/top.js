6dbb20dbd9f14dbcb1ca8ac8089d4bfe391167bf
ailasunsun <aira.5510_1z@icloud.com>
2024-08-02 15:23:57 +0900

    test bykawaguchi

--- 共通川口/js/top.js
+++ 共通川口/js/top.js
@@ -1,15 +1,6 @@
 'use strict';
 
-// ヘッダー用javascript
-// メニュー表示ボタンのjavascript
-document.getElementById('openButton').onclick = function () {
-  const openMenu = document.getElementById('openMenu');
-  openMenu.style.display = 'block';
+document.getElementById("close").onclick = function () {
+  document.getElementById('nav').style.display = 'none';
+  document.getElementById('open').style.display = 'block';
 };
-
-// メニュー非表示ボタンのjavascript
-document.getElementById('closeButton').onclick = function () {
-  const closeMenu = document.getElementById('openMenu');
-  closeMenu.style.display = 'none';
-};
-
