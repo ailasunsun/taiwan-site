44b2d9ec7ddab7d84c67eea832ffec6391d12e7f
ailasunsun <aira.5510_1z@icloud.com>
2024-08-05 11:40:06 +0900

    commonにheader領域を追加しました by川口

--- common/js/others.js
+++ common/js/others.js
@@ -1,7 +1,19 @@
 'use strict';
 
-// navドロワー
-document.getElementById("close").onclick = function () {
-  document.getElementById('nav').style.display = 'none';
-  document.getElementById('open').style.display = 'block';
+// header 領域
+const btn = document.querySelector('.menu-icon');
+const photo = document.getElementById('button');
+let isPhoto1 = true;
+const photo1 = '../common/image/icon_drawer.svg';
+const photo2 = '../common/image/icon_drawer_close.svg';
+
+btn.onclick = function () {
+  if (isPhoto1 === true) {
+    photo.src = photo2;
+    isPhoto1 = false;
+  }
+  else {
+    photo.src = photo1;
+    isPhoto1 = true;
+  }
 };
