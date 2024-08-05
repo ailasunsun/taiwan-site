b0620b3b94c6fc1910e26828a0bcdb7f7b29cac8
ailasunsun <aira.5510_1z@icloud.com>
2024-08-05 11:17:04 +0900

    共通川口コミットしますー

--- 共通川口/js/top.js
+++ 共通川口/js/top.js
@@ -1,6 +1,20 @@
 'use strict';
 
-document.getElementById("close").onclick = function () {
-  document.getElementById('nav').style.display = 'none';
-  document.getElementById('open').style.display = 'block';
+// ドロワーここから
+const btn = document.querySelector('.menu-icon');
+const photo = document.getElementById('button');
+let isPhoto1 = true;
+const photo1 = '../common/image/icon_drawer.svg';
+const photo2 = '../common/image/icon_drawer_close.svg';
+
+btn.onclick = function () {
+  if (isPhoto1 === true) {
+    photo.src = photo2;
+    isPhoto1 = false;
+  }
+  else {
+    photo.src = photo1;
+    isPhoto1 = true;
+  }
 };
+// ドロワーここまで
